4141b4129c6e51d60cc627f4add81e5c55a7a19a
Elsa Touma elsa.touma@gmail.com <elsa.touma@gmail.com>
2020-11-09 16:44:36 +0100

    stupid coach

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,12 @@
+class AnswersController < ApplicationController
+  def answer
+    ask = params[:question]
+    if ask == 'I am going to work right now!'
+      @answer = 'Great!'
+    elsif ask.include?('?')
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
